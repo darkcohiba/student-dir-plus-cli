b8f92f30ff2081f481b97600e80fc16181c85759
Sam <samuel.waters@gmail.com>
2023-03-23 15:55:53 -0600

    chore: fixed seeding and fixed cli to be able to add

--- lib/cli.py
+++ lib/cli.py
@@ -1,16 +1,32 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from db.models import Teacher
+from db.models import Teacher, Student
 
 engine = create_engine('sqlite:///db/studentDir.db')
 session = sessionmaker(bind=engine)()
 
+def create_new_teacher(fname, lname, id):
+    new_teacher = Teacher(
+        teacher_id = id,
+        first_name = fname,
+        last_name = lname,
+    )
+    session.add(new_teacher)
+    session.commit()
 
 if __name__ == '__main__':
+
+    print("Hello, welcome to our student and teacher database!")
+    entry_type = input("Are you entering a new student or teacher? (S/T): ").lower()
+    if entry_type == 't':
+        teachers = session.query(Teacher).all()
+        id = int(len(teachers) + 1)
+        first_name = input('What is your first name')
+        last_name = input('What is your last name')
+        create_new_teacher(first_name, last_name, id)
+    elif entry_type == 's':
+        pass
+
     
-
-
-    teachers = session.query(Teacher).all()
-    print(teachers)
 
--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+import random
 from models import Student, Teacher
 
 
@@ -11,39 +11,70 @@ if __name__ == '__main__':
     session = Session()
 
     # clear database
-    # session.query(Student).delete()
-    # session.query(Teacher).delete()
+    session.query(Student).delete()
+    session.query(Teacher).delete()
 
-    # print("Seeding teachers...")
+    print("Seeding teachers...")
+    teachers = []
+    sam = Teacher(
+        teacher_id = 1,
+        first_name = 'Sam',
+        last_name = 'Waters'
+    )
+    teachers.append(sam)
+    stephen = Teacher(
+        teacher_id = 2,
+        first_name = 'Stephen',
+        last_name = 'Lamp'
+    )
+    teachers.append(stephen)
 
-    # sam = Teacher(
-    #     teacher_id = 1,
-    #     first_name = 'Sam',
-    #     last_name = 'Waters'
-    # )
-    
-    # stephen = Teacher(
-    #     teacher_id = 2,
-    #     first_name = 'Stephen',
-    #     last_name = 'Lamp'
-    # )
+    yesenia = Teacher(
+        teacher_id = 3,
+        first_name = 'Yes Queen',
+        last_name = 'Crown'
+    )
+    teachers.append(stephen)
 
-    # teachers = [sam, stephen]
+    session.add_all(teachers)
+    session.commit()
 
-    # print("Seeding students...")
+    print("Finished seeding Teachers...")
+    print("Seeding students...")
+
+    # teachers = session.query(Teacher).first()
+    students = []
+    student_ids = list(range(1, 11))
+    student_first_names = ["Chase", "Max", "Yesenia", "Josh"]
+    student_last_names = ["Walsh", "Welsh", "Ray", "Rey"]
+    for teacher in teachers:
+        # for i in range(random.randint(3, 5)):
+        student = Student(
+            student_id=student_ids[0],
+            first_name=random.choice(student_first_names),
+            last_name=random.choice(student_last_names),
+            teacher_id=teacher.teacher_id,
+        )
+        student_ids.remove(student.student_id)
+        student_first_names.remove(student.first_name)
+        student_last_names.remove(student.last_name)
+        students.append(student)
+        session.add(student)
+        session.commit()
+
 
     # chase = Student(
     #     student_id = 1,
     #     first_name = 'Chase',
     #     last_name = 'Walsh',
-    #     teacher_id = sam
+    #     teacher_id = 1
     # )
 
     # seth = Student(
     #     student_id = 1,
     #     first_name = 'Chase',
     #     last_name = 'Walsh',
-    #     teacher_id = sam
+    #     teacher_id = 2
     # )
 
     # students = [chase, seth]
